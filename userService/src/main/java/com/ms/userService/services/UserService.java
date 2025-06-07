b90f9e60ee2790b7c4eef7ab28ac9d5d9ec680fc
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-06-07 18:35:45 -0300

    publish message

--- userService/src/main/java/com/ms/userService/services/UserService.java
+++ userService/src/main/java/com/ms/userService/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.userService.services;
 
 import com.ms.userService.models.UserModel;
+import com.ms.userService.producers.UserProducer;
 import com.ms.userService.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,14 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 
 }

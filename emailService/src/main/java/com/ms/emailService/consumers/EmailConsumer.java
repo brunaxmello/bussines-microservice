8e04fdd0c975e5c6fa173fa8904e6eb0f232672c
brunaxmello <bruna.mello@maisunifacisa.com.br>
2025-06-08 02:19:07 -0300

    SpringSMTP integraçcao com email, metodos emailService, e o restante de suas funcionalidades

--- emailService/src/main/java/com/ms/emailService/consumers/EmailConsumer.java
+++ emailService/src/main/java/com/ms/emailService/consumers/EmailConsumer.java
@@ -2,20 +2,26 @@ package com.ms.emailService.consumers;
 
 import com.ms.emailService.dtos.EmailRecordDto;
 import com.ms.emailService.models.EmailModel;
+import com.ms.emailService.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(emailRecordDto, emailModel);
-        //sendEmail
+        emailService.sendEmail(emailModel);
 
     }
 
